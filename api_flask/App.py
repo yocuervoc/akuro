dc425b7b09430a10f678cdf4e00a32f292428121
yocuervoc <yocuervoc@unal.edu.co>
2020-07-10 22:55:02 -0500

    server

--- api_flask/App.py
+++ api_flask/App.py
@@ -3,10 +3,10 @@ from flask import request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSLQ_HOST'] = 'localhost'
-app.config['MYSLQ_USER'] = 'root'
-app.config['MYSLQ_PASSWORD'] = 'password'
-app.config['MYSLQ_DB'] = 'akuro'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'akuro'
 
 mysql = MySQL(app)
 
@@ -17,6 +17,9 @@ def procesing_data():
         amount = data["amount"]
         id = data["id"]
         data = {'success': 'ok', 'total': (amount*id)/4}
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO transanction values(%s, %s)', (amount, id))
+        mysql.connection.commit()
         return make_response(jsonify(data), 201)
     else:
         data = {'error'}
