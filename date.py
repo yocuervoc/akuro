552c1f10345a307b61a883f3fd3f40bb648ba634
yocuervoc <yocuervoc@unal.edu.co>
2020-07-10 22:23:35 -0500

    fecha JSON

--- date.py
+++ date.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+
 def date_calculator(json):
 
     json_parts = json.split('\"')
@@ -7,4 +8,5 @@ def date_calculator(json):
     date = datetime.datetime(int(str_date[0]), int(str_date[1]), int(str_date[2]))
     
     return (date + datetime.timedelta(days=20))
+
     
