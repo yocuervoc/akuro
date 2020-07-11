ffae9255c2139042ea0328931cd252fea1684f18
yocuervoc <yocuervoc@unal.edu.co>
2020-07-10 23:26:44 -0500

    cors

--- api_flask/App.py
+++ api_flask/App.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response
 from flask import request
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -8,6 +9,8 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'akuro'
 
+CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 mysql = MySQL(app)
 
 @app.route('/procesingEndpoint', methods=['POST'])
@@ -18,7 +21,7 @@ def procesing_data():
         id = data["id"]
         data = {'success': 'ok', 'total': (amount*id)/4}
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO transanction values(%s, %s)', (amount, id))
+        cur.execute('INSERT INTO transanction values(%s, %s)', (id, amount))
         mysql.connection.commit()
         return make_response(jsonify(data), 201)
     else:
